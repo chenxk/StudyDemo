eee9a8eb3992349f54754d1f7f68365e22a8c539
chenxk <826179140@qq.com>
2018-05-23 23:36:24 +0800

    kNN算法学习一把

--- com/study/ai/ms/kNNTest.py
+++ com/study/ai/ms/kNNTest.py
@@ -27,22 +27,22 @@ def classify0(inX, dataSet, labels, k):
 　　A的类型众多，几乎所有类型都可以：array, list, tuple, dict, matrix以及基本数据类型int, string, float以及bool类型。
 　　reps的类型也很多，可以是tuple，list, dict, array, int, bool.但不可以是float, string, matrix类型。
     '''
-    #行 列
+    # 行 列
     xDataSet = tile(inX, (dataSetSize, 1))
     print(xDataSet)
-    #矩阵相减
+    # 矩阵相减
     diffMat = xDataSet - dataSet
     print(diffMat)
-    #取平方
+    # 取平方
     sqDiffMat = diffMat ** 2
     print(sqDiffMat)
-    #矩阵同行相加，得到一维数组
+    # 矩阵同行相加，得到一维数组
     sqDistances = sqDiffMat.sum(axis=1)
     print(sqDistances)
-    #数组每个元素开根号
+    # 数组每个元素开根号
     distances = sqDistances ** 0.5
     print(distances)
-    #argsort函数返回的是数组值从小到大的索引值
+    # argsort函数返回的是数组值从小到大的索引值
     sortedDistIndicies = distances.argsort()
     print(sortedDistIndicies)
     # 定义一个字典
@@ -53,16 +53,19 @@ def classify0(inX, dataSet, labels, k):
         # 返回指定键的值，如果值不在字典中返回default值
         tempLabel = classCount.get(votelLabel, 0)
         print(tempLabel)
-        #将符合的结果放入字典中
+        # 将符合的结果放入字典中
         classCount[votelLabel] = tempLabel + 1
-    #符合的结果
+    # 符合的结果
     print(classCount)
-    #以列表返回可遍历的(键, 值) 元组数组
-    #print(classCount.items())
+    # 以列表返回可遍历的(键, 值) 元组数组
+    # print(classCount.items())
     # operator模块提供的itemgetter函数用于获取对象的哪些维的数据，参数为一些序号
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     print(sortedClassCount)
     return sortedClassCount[0][0]
 
 
-print(classify0(3, group, labels, 3))
+print(classify0(0.2, group, labels, 3))
+print(classify0(2, group, labels, 3))
+print(classify0([0.2, 0.5], group, labels, 3))
+print(classify0([0.9, 1.5], group, labels, 3))
